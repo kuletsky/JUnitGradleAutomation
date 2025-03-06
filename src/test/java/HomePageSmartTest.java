b8050b02a658f6eafcd6ac247e3bf0b831d89b4d
kuletsky <kuletsky@gmail.com>
2025-03-06 12:13:06 -0500

    test

--- src/test/java/HomePageSmartTest.java
+++ src/test/java/HomePageSmartTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
