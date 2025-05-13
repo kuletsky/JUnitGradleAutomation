b5d03fd15df4f188faacdd5b9980ffa99936133d
kuletsky <150550546+kuletsky@users.noreply.github.com>
2025-05-12 20:12:14 -0400

    Kk/refactoring yml add selenium (#12)
    * refactoring to chaining
    
    * add remote selenium

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -3,16 +3,22 @@ package ui;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 public class BaseTest {
     WebDriver driver;
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        initDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
@@ -20,5 +26,25 @@ public class BaseTest {
     @AfterEach
     void tearDown() {
         driver.quit();
+    }
+
+    private void initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        System.out.println("SELENIUM_REMOTE_URL = " + remoteUrl);
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
     }
 }
